860d719b3f37c7ed70b0016e5e25c422c73d3aaa
Christophe Raffalli <christophe@raffalli.eu>
2021-10-15 02:51:08 +0000

    my first module

---
+++ test/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+
+class MyModule(http.Controller):
+     @http.route('/test/test/', auth='public')
+     def index(self, **kw):
+         return "Hello, world"
+
+     @http.route('/test/test/objects/', auth='public')
+     def list(self, **kw):
+         return http.request.render('test.listing', {
+             'root': '/test/test',
+             'objects': http.request.env['test'].search([]),
+         })
+
+#     @http.route('/my_module/my_module/objects/<model("my_module.my_module"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('my_module.object', {
+#             'object': obj
+#         })
